28bccb2ce02253aa09afdc8f1dfbb10e989e52b5
Jordan Caudill <jcaudill@ESTECH.com>
2017-01-26 12:10:33 -0600

    qwerq

--- googlesheet/googlesheet.js
+++ googlesheet/googlesheet.js
@@ -18,7 +18,7 @@ function googlesheet() {
     var TOKEN_PATH = TOKEN_DIR + 'drive-nodejs-quickstart.json';
 
 // Load client secrets from a local file.
-    fs.readFile('services/googlesheet/client_secret.json', function processClientSecrets(err, content) {
+    fs.readFile('./googlesheet/client_secret.json', function processClientSecrets(err, content) {
         if (err) {
             console.log('Error loading client secret file: ' + err);
             return;
